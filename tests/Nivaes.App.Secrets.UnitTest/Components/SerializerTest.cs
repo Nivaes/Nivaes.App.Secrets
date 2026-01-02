08953a9aee2f334997ccef748f26cd369bb05f4c
gibhub action <action@github.com>
2026-01-02 12:18:42 +0100

    Use fscheck

--- tests/Nivaes.App.Secrets.UnitTest/Components/SerializerTest.cs
+++ tests/Nivaes.App.Secrets.UnitTest/Components/SerializerTest.cs
@@ -1,4 +1,5 @@
-﻿using Nivaes.App.Secrets.SourceGenerator;
+﻿using FsCheck.Xunit;
+using Nivaes.App.Secrets.SourceGenerator;
 using Nivaes.DataTestGenerator.Xunit;
 using Shouldly;
 
@@ -24,7 +25,6 @@ public class SerializerTest
     [Fact]
     public void Serializa_secris()
     {
-        var aa = new Lazy<byte[]>(() => { return new byte[] { 0, 2, 3, 2, 3 }; });
         SecretMocks secrets1 = new SecretMocks()
         {
             Secret1 = "hola",
@@ -53,7 +53,6 @@ public class SerializerTest
     [GenerateStringInlineData(50, MinSize = 50, MaxSize = 1000)]
     public void Serialize_secret_multi(string secret)
     {
-        var aa = new Lazy<byte[]>(() => { return new byte[] { 0, 2, 3, 2, 3 }; });
         SecretMocks secrets1 = new SecretMocks()
         {
             Secret1 = "hola",
@@ -77,5 +76,26 @@ public class SerializerTest
         secrets1.Secret2.ShouldBe(secrets2.Secret2);
         secrets1.Secret3.ShouldBe(secrets2.Secret3);
         secrets1.Secret3.ShouldBe(secret);
+    }
+
+    [Property(MaxTest = 500)]
+    public void Serialize_secret_fscheck(SecretMocks secrets)
+    {
+        var codes = SecretsSerializationHelper.Serializer(new[] { secrets.Secret1, secrets.Secret2, secrets.Secret3 });
+
+        var codes2 = SecretsSerializer.Deserialize(codes).ToArray();
+
+        _output.WriteLine(string.Join(",", codes));
+
+        SecretMocks secretsCopy = new SecretMocks()
+        {
+            Secret1 = codes2[0],
+            Secret2 = codes2[1],
+            Secret3 = codes2[2],
+        };
+
+        secretsCopy.Secret1.ShouldBe(secrets.Secret1);
+        secretsCopy.Secret2.ShouldBe(secrets.Secret2);
+        secretsCopy.Secret3.ShouldBe(secrets.Secret3);
     }
 }
