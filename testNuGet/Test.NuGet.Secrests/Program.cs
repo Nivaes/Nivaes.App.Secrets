4fb6d03bde231ef7681a0d1e9fb71c3a4277c3df
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-01-02 16:25:58 +0100

    Include Test NuGet

---
+++ testNuGet/Test.NuGet.Secrests/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.NuGet.Secrets
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Test.NuGet.Secrets.Secrets.AppDns);
+            Console.WriteLine(Test.NuGet.Secrets.Secrets.SentryDns);
+        }
+    }
+}
